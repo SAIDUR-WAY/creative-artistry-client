da4a1e3b1fdb20b9937faf39cbe7916c42070155
saidur-way <saiduronway@gmail.com>
2023-06-09 02:47:23 +0600

    envrment veriable added

--- src/firebaseConfig/firebase.config.js
+++ src/firebaseConfig/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDq7K5SpZEuSb1lihEq46UkPiW-cSgsrs",
-  authDomain: "creative-artistry-client.firebaseapp.com",
-  projectId: "creative-artistry-client",
-  storageBucket: "creative-artistry-client.appspot.com",
-  messagingSenderId: "524282783680",
-  appId: "1:524282783680:web:7d8e45897ce4b3bd6c8a25"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
